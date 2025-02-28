48734d5e684e656ee9cfceb51b00f64a369bb124
unknown <jisu1008@inha.edu>
2025-02-28 11:11:18 +0900

    v1.3 day13 ml assigment

--- day13.py
+++ day13.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error, r2_score
 
-data = [1, 7, 5, 2, 8, 3, 6, 4]
-bins = [0, 3, 6, 9]
-labels = ["low", "mid", "high"]
-cat = pd.cut(data, bins, labels=labels, right=False)
-print(cat)
+mpg = sns.load_dataset('mpg')
+print(mpg.info())
