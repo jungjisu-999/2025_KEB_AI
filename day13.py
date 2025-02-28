2b65ff963e41f73c38abe6bf0675f783f710a79a
unknown <jisu1008@inha.edu>
2025-02-28 11:12:30 +0900

    v1.4 day13 ml assigment

--- day13.py
+++ day13.py
@@ -1,3 +1,7 @@
+# Assignment
+# 데이터 로딩 -> 데이터 전처리 -> 타겟 및 독립변수 설정 -> 트레이닝/테스트 셋 설정
+# -> 모델 선택 및 학습 -> 예측 수행 -> 성능 평가(mse 등) -> 시각화
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,4 +10,6 @@ from sklearn.model_selection import train_test_split
 # from sklearn.metrics import mean_squared_error, r2_score
 
 mpg = sns.load_dataset('mpg')
-print(mpg.info())
+# print(mpg.info())
+# print(mpg[["horsepower"]].tail(10))
+# print(mpg.tail(10))
